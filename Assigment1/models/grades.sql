c02e13fd04b8a0aceae82c43d668c92260e2be8b
SarahPauwels-ucll <r0875579@ucll.be>
2024-03-29 11:23:31 +0100

    grades

--- Assigment1/models/grades.sql
+++ Assigment1/models/grades.sql
@@ -1,1 +1,4 @@
-a
+{{ config(materialized='external', location='output/fact_grades.csv') }}
+
+SELECT id_assessment,id_student, score, code_module
+FROM {{source('raw_data', 'studentAssessment')}} full outer join {{source('raw_data', 'assessments')}} using(id_assessment)
